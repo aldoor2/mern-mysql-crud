ad5002b7bc74979e87f6a7ec5928400b337de7cf
Alphonse <apuertaj2@gmail.com>
2022-08-26 06:24:14 -0500

    Added module morgan to rest API

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 
 import { PORT } from './config.js'
 
@@ -12,6 +13,7 @@ const app = express()
 // Middlewares
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 // Routes
 app.use(indexRouter)
